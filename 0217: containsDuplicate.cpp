6db6be9ba634e75d89f5485caf03404f7a5e5917
Jorge <55205859+jmartinez159@users.noreply.github.com>
2023-03-14 22:55:28 -0700

    Using set to look for dup
    Problem Description: https://leetcode.com/problems/contains-duplicate/description/
    Set size is compared with vector size to check if there is a duplicate int.
    If sizes aren't equal then a dup is present.

---
+++ 0217: containsDuplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        set<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+
+            ans.insert(nums[i]);
+        }
+
+        return ans.size() != nums.size(); 
+    }
+};
