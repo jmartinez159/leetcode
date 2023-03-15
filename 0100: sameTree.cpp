c7e0c9b43ffd352c8bc28b3fa74541a93907f1f3
Jorge <55205859+jmartinez159@users.noreply.github.com>
2023-03-15 08:43:20 -0700

    Using recursion to literate through trees
    Problem Description: https://leetcode.com/problems/same-tree/description/
    Using recursion to check if both trees are the same if values and existing nodes are the same.

---
+++ 0100: sameTree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == nullptr && q == nullptr){
+
+            return true;
+        }
+
+        if(p != nullptr && q != nullptr){
+
+            if(p->val != q->val){
+
+                return false;
+            }
+
+            //checking to see if p->left matches q->left AND p->right matches q->right
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+
+        return false;
+    }
+};
