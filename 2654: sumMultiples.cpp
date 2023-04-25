9c2385df9cbb790ef33323b038dc360570a85bd4
Jorge <55205859+jmartinez159@users.noreply.github.com>
2023-04-25 00:49:31 -0700

    Easy - 4/24/2023 - T: 2m50s

---
+++ 2654: sumMultiples.cpp
@@ -0,0 +1,26 @@
+// problem : https://leetcode.com/problems/sum-multiples/submissions/
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+
+            if(i%3 == 0){
+
+                sum += i;
+            }
+
+            else if(i%5 == 0){
+
+                sum += i;
+            }
+            else if(i%7 == 0){
+
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
