0e65c463d34a1fad6fe947eb2201d3a43bcea7e6
Jorge <jmx012@gmail.com>
2024-01-23 16:52:38 -0800

    Solved 367 in Python

---
+++ 0367: validPerfectSquare.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low, high = 0, num
+        while low <= high:
+            mid = int((low+high)/2)
+            if mid*mid == num:
+                return True
+            elif mid*mid > num:
+                high = mid-1
+            else:
+                low = mid+1
+        return False
