8d601ee0c2325cfde7709cccd9e7a9627d568756
Jorge <jmx012@gmail.com>
2024-02-29 22:54:00 -0800

    Solved 56 in Python [10m27s]

---
+++ 0056: mergeIntervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort()
+        curr = -1
+        for x,y in intervals:
+            print(x, curr)
+            if x > curr:
+                res.append([x,y])
+            elif res[len(res)-1][1] > y:
+                continue
+            else:
+                res[len(res)-1][1] = y
+            curr = y
+        return res
